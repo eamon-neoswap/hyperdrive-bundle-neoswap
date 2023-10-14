2b031873f921da6b254c87c62165ff2b6f306827
Eamon Penland <eamon.penland@neoswap.ai>
2023-10-14 11:54:49 -0500

    added price info

--- app/_components/Bundle.tsx
+++ app/_components/Bundle.tsx
@@ -1,3 +1,4 @@
+import { LAMPORTS_PER_SOL } from "@solana/web3.js";
 import { data } from "../_data";
 
 interface Props {
@@ -6,6 +7,45 @@ interface Props {
   setOrderQuantity: (data: any) => void;
   orderQuantity: { [itemId: string]: number };
   isSubmitting: boolean;
+  priceData: any;
+  fetchingPrices: boolean;
+}
+
+function getPriceInfo(mint: string, priceData: any) {
+  let label: string = "--";
+  let price = 0;
+  priceData?.itemsData?.forEach((ele: { mint: string; price: number }) => {
+    if (ele.mint === mint) {
+      price += ele.price;
+    }
+  });
+
+  if (price > 0) {
+    label = `${price}`;
+  }
+  return label;
+}
+
+function showSpinner() {
+  return (
+    <svg
+      aria-hidden="true"
+      role="status"
+      className="inline w-4 h-4 mr-3 animate-spin"
+      viewBox="0 0 100 101"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+        fill="#E5E7EB"
+      />
+      <path
+        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
 }
 
 export default function Bundle({
@@ -14,7 +54,10 @@ export default function Bundle({
   setOrderQuantity,
   orderQuantity,
   isSubmitting,
+  priceData,
+  fetchingPrices,
 }: Props) {
+  console.log("PRICE DATA", priceData);
   return (
     <div className="w-full flex flex-col gap-10">
       <div className="flex flex-row w-full rounded-2xl p-2 gap-4 flex-wrap justify-center">
@@ -38,23 +81,23 @@ export default function Bundle({
                 </div>
               </div>
               <div className="px-3 py-[10.58px] flex-col justify-start items-start gap-2 flex w-full">
-                <div className="self-stretch text-white text-[13.885246276855469px] font-bold leading-tight">
+                <div className="self-stretch text-white text-[13px] font-bold leading-tight">
                   {ele.name}
                 </div>
                 <div className="self-stretch justify-start items-start gap-[6.61px] inline-flex">
-                  <div className="grow shrink basis-0 text-white text-[10.579235076904297px] font-normal">
+                  <div className="grow shrink basis-0 text-white text-[10px] font-normal">
                     {ele.collection.collection}
                   </div>
-                  <div className="text-right text-red-600 text-[10.579235076904297px] font-normal">
-                    <div
-                      role="button"
-                      className="text-white text-[10.579235076904297px]"
-                    >
+                  <div className="text-right text-red-600 text-[10px] font-normal">
+                    <div role="button" className="text-white text-[10px]">
                       <a target="_blank" href={ele.marketplace.url}>
                         View more details
                       </a>
                     </div>
                   </div>
+                </div>
+                <div className="self-stretch justify-start items-start gap-[6.61px] inline-flex text-white text-[10px]">
+                  <span>Price: {getPriceInfo(ele.mint, priceData)}</span>
                 </div>
               </div>
 
@@ -81,19 +124,26 @@ export default function Bundle({
           );
         })}
       </div>
-      <div className="flex w-full justify-center">
+      <div className="flex flex-col w-full gap-2 text-white items-center">
+        <div>Price Estimates</div>
+        <div>Sol: {priceData?.atlasData?.sol ?? '--'}</div>
+        <div>Atlas: {priceData?.atlasData?.atlas ?? '--'}</div>
+      </div>
+      <div className="flex w-full justify-center gap-5">
         <button
           onClick={getPrice}
           className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-4 px-6 border border-blue-500 hover:border-transparent rounded"
-          disabled={isSubmitting}
+          disabled={fetchingPrices}
         >
-          estimate prices
+          {fetchingPrices && showSpinner()}
+          Estimate Prices
         </button>
         <button
           onClick={handleSubmit}
           className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-4 px-6 border border-blue-500 hover:border-transparent rounded"
           disabled={isSubmitting}
         >
+          {isSubmitting && showSpinner()}
           Buy Now
         </button>
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -6,233 +6,256 @@ import Image from "next/image";
 import { SetStateAction, useEffect, useState } from "react";
 import Bundle from "./_components/Bundle";
 import { getSolBalance, stateToBuy } from "./services/utils";
-import { getStarAtlasBundle, getStarAtlasPrices, styles } from "./services/starAtlas";
+import {
+  getStarAtlasBundle,
+  getStarAtlasPrices,
+  styles,
+} from "./services/starAtlas";
 import { Wallet } from "@coral-xyz/anchor";
 
 export default function Home() {
-    const [balance, setBalance] = useState(0);
-    const [state, setState] = useState({});
-    const [isSubmitting, setIsSubmitting] = useState(false);
-    const { connected, publicKey, wallet } = useWallet();
-    const { connection } = useConnection();
+  const [balance, setBalance] = useState(0);
+  const [state, setState] = useState({});
+  const [priceData, setPriceData] = useState<any>();
+  const [fetchingPrices, setFetchingPrices] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const { connected, publicKey, wallet } = useWallet();
+  const { connection } = useConnection();
 
-    const setOrderQuantity = (data: { [itemId: string]: number }) => {
-        setState({ ...state, ...data });
+  const setOrderQuantity = (data: { [itemId: string]: number }) => {
+    setState({ ...state, ...data });
+  };
+
+  const roundBalance = (balance: number) => Math.round(balance * 10000) / 10000;
+
+  useEffect(() => {
+    const getBalance = async () => {
+      if (!publicKey) return;
+      try {
+        const fetchedBalance = await getSolBalance(publicKey);
+        console.log("fetched balance", fetchedBalance);
+        setBalance(roundBalance(fetchedBalance / LAMPORTS_PER_SOL));
+      } catch (e) {
+        console.log(`error getting balance: `, e);
+      }
     };
 
-    const roundBalance = (balance: number) => Math.round(balance * 10000) / 10000;
+    getBalance();
+  }, [publicKey, connection]);
 
-    useEffect(() => {
-        const getBalance = async () => {
-            if (!publicKey) return;
-            try {
-                const fetchedBalance = await getSolBalance(publicKey);
-                console.log("fetched balance", fetchedBalance);
-                setBalance(roundBalance(fetchedBalance / LAMPORTS_PER_SOL));
-            } catch (e) {
-                console.log(`error getting balance: `, e);
-            }
-        };
+  const handleSubmit = async () => {
+    if (!publicKey || !wallet) throw "connect wallet";
+    // console.log(state);
 
-        getBalance();
-    }, [publicKey, connection]);
+    if (Object.keys(state).length === 0) {
+      alert("no item selected");
+      throw "no item selected";
+    }
+    try {
+      setIsSubmitting(true);
+      const toBuy = stateToBuy(state);
 
-    const handleSubmit = async () => {
-        if (!publicKey || !wallet) throw "connect wallet";
-        // console.log(state);
+      console.log("toBuy", toBuy);
+      const res = await getStarAtlasBundle({
+        user: publicKey,
+        wallet: wallet.adapter as unknown as Wallet,
+        toBuy,
+      });
+      console.log(res);
+      alert("success");
+      return res;
+    } catch (error) {
+      console.log(error);
 
-        if (Object.keys(state).length === 0) {
-            alert("no item selected");
-            throw "no item selected";
-        }
-        try {
-            setIsSubmitting(true);
-            const toBuy = stateToBuy(state);
+      alert(error);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+  const getPrice = async () => {
+    if (!publicKey || !wallet) throw "connect wallet";
+    // console.log(state);
 
-            console.log("toBuy", toBuy);
-            const res = await getStarAtlasBundle({
-                user: publicKey,
-                wallet: wallet.adapter as unknown as Wallet,
-                toBuy,
-            });
-            console.log(res);
-            alert("success");
-            return res;
-        } catch (error) {
-            console.log(error);
+    if (Object.keys(state).length === 0) {
+      alert("no item selected");
+      throw "no item selected";
+    }
+    try {
+      // setIsSubmitting(true);
+      setFetchingPrices(true);
+      const toBuy = stateToBuy(state);
 
-            alert(error);
-        } finally {
-            setIsSubmitting(false);
-        }
-    };
-    const getPrice = async () => {
-        if (!publicKey || !wallet) throw "connect wallet";
-        // console.log(state);
+      console.log("toBuy", toBuy);
 
-        if (Object.keys(state).length === 0) {
-            alert("no item selected");
-            throw "no item selected";
-        }
-        try {
-            // setIsSubmitting(true);
-            const toBuy = stateToBuy(state);
+      const res = await getStarAtlasPrices({
+        user: publicKey,
+        // wallet: wallet.adapter as unknown as Wallet,
+        toBuy,
+      });
+      console.log(res);
+      setPriceData(res);
+      return res;
+    } catch (error) {
+      console.log(error);
 
-            console.log("toBuy", toBuy);
+      alert(error);
+    } finally {
+      setFetchingPrices(false);
+    }
+  };
+  const [selectedOption, setSelectedOption] = useState("STAR ATLAS");
 
-            const res = await getStarAtlasPrices({
-                user: publicKey,
-                // wallet: wallet.adapter as unknown as Wallet,
-                toBuy,
-            });
-            console.log(res);
+  const handleOptionChange = (event: {
+    target: { value: SetStateAction<string> };
+  }) => {
+    setSelectedOption(event.target.value);
+  };
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24 gap-5">
+      <div className="z-10 max-w-5xl w-full items-center justify-center font-mono text-sm lg:flex">
+        <a
+          className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+          href="https://neoswap.xyz"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            src="/smallLogo.svg"
+            alt="Neoswap"
+            width={100}
+            height={24}
+            priority
+          />
+        </a>
+      </div>
+      <div>
+        <WalletMultiButton />
+      </div>
+      <div>
+        <label
+          htmlFor="menu"
+          style={{
+            backgroundColor: "lightblue",
+            padding: "11px",
+            border: "none",
+            borderRadius: "10px",
+          }}
+        >
+          {"Select an Game =>"}
+        </label>
+        ...
+        <select
+          id="menu"
+          value={selectedOption}
+          onChange={handleOptionChange}
+          style={{
+            backgroundColor: "lightblue",
+            padding: "10px",
+            border: "none",
+            borderRadius: "10px",
+          }}
+        >
+          <option value="STAR ATLAS">STAR ATLAS</option>
+        </select>
+      </div>
 
-            return res;
-        } catch (error) {
-            console.log(error);
+      {connected && (
+        <>
+          <div className="text-white">
+            <span className="uppercase font-bold">Balance:</span> {balance} SOL
+          </div>
 
-            alert(error);
-        } finally {
-            setIsSubmitting(false);
-        }
-    };
-    const [selectedOption, setSelectedOption] = useState("STAR ATLAS");
+          <Bundle
+            setOrderQuantity={setOrderQuantity}
+            handleSubmit={handleSubmit}
+            getPrice={getPrice}
+            orderQuantity={state}
+            isSubmitting={isSubmitting}
+            priceData={priceData}
+            fetchingPrices={fetchingPrices}
+          />
+        </>
+      )}
+      <div style={styles.container}>
+        <h1 style={styles.header}>
+          Welcome to NeoSwap - Your Solana Games Companion
+        </h1>
+        <br />
+        <h2 style={styles.instructionHeader}>Instructions:</h2>
 
-    const handleOptionChange = (event: { target: { value: SetStateAction<string> } }) => {
-        setSelectedOption(event.target.value);
-    };
-    return (
-        <main className="flex min-h-screen flex-col items-center p-24 gap-5">
-            <div className="z-10 max-w-5xl w-full items-center justify-center font-mono text-sm lg:flex">
-                <a
-                    className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-                    href="https://neoswap.xyz"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image src="/smallLogo.svg" alt="Neoswap" width={100} height={24} priority />
-                </a>
-            </div>
-            <div>
-                <WalletMultiButton />
-            </div>
-            <div>
-                <label
-                    htmlFor="menu"
-                    style={{
-                        backgroundColor: "lightblue",
-                        padding: "11px",
-                        border: "none",
-                        borderRadius: "10px",
-                    }}
-                >
-                    {"Select an Game =>"}
-                </label>
-                ...
-                <select
-                    id="menu"
-                    value={selectedOption}
-                    onChange={handleOptionChange}
-                    style={{
-                        backgroundColor: "lightblue",
-                        padding: "10px",
-                        border: "none",
-                        borderRadius: "10px",
-                    }}
-                >
-                    <option value="STAR ATLAS">STAR ATLAS</option>
-                </select>
-            </div>
+        <ol>
+          <li style={styles.instructions}>
+            1) Connect your wallet by clicking on *Select Wallet* on the top of
+            the page
+          </li>
+          <li style={styles.instructions}>
+            2) Select your game of choice in the dropdown menu
+          </li>
+          <li style={styles.instructions}>
+            3) Input in the according fields the required quantities of the
+            specific items you want to buy
+          </li>
+          <li style={styles.instructions}>
+            3) Click on *Buy* and let magic happens
+          </li>
+        </ol>
 
-            {connected && (
-                <>
-                    <div className="text-white">
-                        <span className="uppercase font-bold">Balance:</span> {balance} SOL
-                    </div>
+        <br />
+        <br />
 
-                    <Bundle
-                        setOrderQuantity={setOrderQuantity}
-                        handleSubmit={handleSubmit}
-                        getPrice={getPrice}
-                        orderQuantity={state}
-                        isSubmitting={isSubmitting}
-                    />
-                </>
-            )}
-            <div style={styles.container}>
-                <h1 style={styles.header}>Welcome to NeoSwap - Your Solana Games Companion</h1>
-                <br />
-                <h2 style={styles.instructionHeader}>Instructions:</h2>
+        <p style={styles.paragraph}>
+          As a dedicated gamer, I've often found myself in dire straits within
+          StarAtlas—stranded in the cosmic abyss, lacking essentials like fuel,
+          ammo, and food.
+        </p>
+        <p style={styles.paragraph}>
+          It was cumbersome to scroll the galactic market place to find the
+          cheapest resources that can extend my expedition in the fastest way.
+        </p>
+        <br />
+        <p style={styles.paragraph}>
+          Fortunately, at NeoSwap we came up with an idea. We were empowered to
+          craft a tool for the HyperDrive hackathon, enabling bulk purchases of
+          vital items and resources.
+        </p>
+        <p style={styles.paragraph}>
+          The tool excelled at sourcing the cheapest orders, securing the best
+          deals with minimal effort.
+        </p>
+        <br />
+        <p style={styles.paragraph}>
+          The power of this tool is its streamlined transactions with a
+          one-signature feature, saving precious time.
+        </p>
+        <br />
+        <p style={styles.paragraph}>
+          With NeoSwap's help, I'm conquering the galaxy, turning the tables on
+          adversity, all while embracing my lazy gamer spirit.
+        </p>
 
-                <ol>
-                    <li style={styles.instructions}>
-                        1) Connect your wallet by clicking on *Select Wallet* on the top of the page
-                    </li>
-                    <li style={styles.instructions}>
-                        2) Select your game of choice in the dropdown menu
-                    </li>
-                    <li style={styles.instructions}>
-                        3) Input in the according fields the required quantities of the specific
-                        items you want to buy
-                    </li>
-                    <li style={styles.instructions}>3) Click on *Buy* and let magic happens</li>
-                </ol>
+        <br />
+        <br />
 
-                <br />
-                <br />
+        <h2 style={styles.faqHeader}>FAQ:</h2>
 
-                <p style={styles.paragraph}>
-                    As a dedicated gamer, I've often found myself in dire straits within
-                    StarAtlas—stranded in the cosmic abyss, lacking essentials like fuel, ammo, and
-                    food.
-                </p>
-                <p style={styles.paragraph}>
-                    It was cumbersome to scroll the galactic market place to find the cheapest
-                    resources that can extend my expedition in the fastest way.
-                </p>
-                <br />
-                <p style={styles.paragraph}>
-                    Fortunately, at NeoSwap we came up with an idea. We were empowered to craft a
-                    tool for the HyperDrive hackathon, enabling bulk purchases of vital items and
-                    resources.
-                </p>
-                <p style={styles.paragraph}>
-                    The tool excelled at sourcing the cheapest orders, securing the best deals with
-                    minimal effort.
-                </p>
-                <br />
-                <p style={styles.paragraph}>
-                    The power of this tool is its streamlined transactions with a one-signature
-                    feature, saving precious time.
-                </p>
-                <br />
-                <p style={styles.paragraph}>
-                    With NeoSwap's help, I'm conquering the galaxy, turning the tables on adversity,
-                    all while embracing my lazy gamer spirit.
-                </p>
-
-                <br />
-                <br />
-
-                <h2 style={styles.faqHeader}>FAQ:</h2>
-
-                <p style={styles.faq}>
-                    <strong>-- What is happening?</strong>
-                </p>
-                <p style={styles.faq}>
-                    Our bot scans Star Atlas Galactic Market to find the cheapest items and compiles
-                    everything in a bundle of transactions that only requires one signature. Neoswap
-                    Bundle is the fastest and easiest way to buy your resources on Star Atlas.
-                </p>
-                <br />
-                <p style={styles.faq}>
-                    <strong>-- Will there be other games integrated?</strong>
-                </p>
-                <p style={styles.faq}>
-                    Yes, we plan to standardize Marketplaces interaction with a suite of tools to
-                    allow users to make their custom bundle on games on Solana.
-                </p>
-            </div>
-        </main>
-    );
+        <p style={styles.faq}>
+          <strong>-- What is happening?</strong>
+        </p>
+        <p style={styles.faq}>
+          Our bot scans Star Atlas Galactic Market to find the cheapest items
+          and compiles everything in a bundle of transactions that only requires
+          one signature. Neoswap Bundle is the fastest and easiest way to buy
+          your resources on Star Atlas.
+        </p>
+        <br />
+        <p style={styles.faq}>
+          <strong>-- Will there be other games integrated?</strong>
+        </p>
+        <p style={styles.faq}>
+          Yes, we plan to standardize Marketplaces interaction with a suite of
+          tools to allow users to make their custom bundle on games on Solana.
+        </p>
+      </div>
+    </main>
+  );
 }
